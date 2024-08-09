542d9550132122111b13fe849edd1950c6fc95c2
ngocanh2704 <ngocanh27041994@gmail.com>
2024-08-10 02:10:00 +0700

    disablle utility.js

--- routes/utility.js
+++ routes/utility.js
@@ -1,21 +1,21 @@
-var express = require('express')
-const { getAllUtilityController, createUtilityController, deleteUtilityController, editUtilityController } = require('../controllers/UtilityController')
-var router =  express.Router()
+// var express = require('express')
+// const { getAllUtilityController, createUtilityController, deleteUtilityController, editUtilityController } = require('../controllers/UtilityController')
+// var router =  express.Router()
 
-// @route GET Utility
-// @desct Lấy tất cả tiện ích
-router.get('/',getAllUtilityController)
+// // @route GET Utility
+// // @desct Lấy tất cả tiện ích
+// router.get('/',getAllUtilityController)
 
-// @route POST create Utility
-// @desc Tạo mới tiện ích
-router.post('/create', createUtilityController)
+// // @route POST create Utility
+// // @desc Tạo mới tiện ích
+// router.post('/create', createUtilityController)
 
-// @route POST delete Utility
-// @desc Xoá tiện ích
-router.post('/delete',deleteUtilityController)
+// // @route POST delete Utility
+// // @desc Xoá tiện ích
+// router.post('/delete',deleteUtilityController)
 
-// @route POST edit Utility
-// @desc Sửa tiện ích
-router.post('/edit',  editUtilityController)
+// // @route POST edit Utility
+// // @desc Sửa tiện ích
+// router.post('/edit',  editUtilityController)
 
-module.exports = router
+// module.exports = router
